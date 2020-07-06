1df3527d983fe0660d919abb14f59e173addab23
Ritesh Bhadane <rmbhadane33@gmail.com>
2020-07-06 19:43:39 +0530

    Update MainActivity.java

--- app/src/main/java/in/inator/greenscreenman/MainActivity.java
+++ app/src/main/java/in/inator/greenscreenman/MainActivity.java
@@ -1,14 +1,38 @@
 package in.inator.greenscreenman;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    static TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button);
+
+        //check if permission is granted or not
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA},1000);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opens new Activity to capture qr code
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            }
+        });
     }
 }
