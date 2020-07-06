aae03f90f630fa2bec2054708fce0b252a1fd99e
Ritesh Bhadane <rmbhadane33@gmail.com>
2020-07-06 19:44:52 +0530

    Update MainActivity2.java

--- app/src/main/java/in/inator/greenscreenman/MainActivity2.java
+++ app/src/main/java/in/inator/greenscreenman/MainActivity2.java
@@ -1,13 +1,38 @@
 package in.inator.greenscreenman;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.zxing.Result;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity2 extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    ZXingScannerView scannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        MainActivity.textView.setText(result.getText());
+        onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
     }
 }
