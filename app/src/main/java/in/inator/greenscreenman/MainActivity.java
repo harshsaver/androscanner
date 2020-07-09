9128520a8f4e6e9dca67ac08e85a42d7715c5269
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2020-07-09 15:20:32 +0530

    Interactivity

--- app/src/main/java/in/inator/greenscreenman/MainActivity.java
+++ app/src/main/java/in/inator/greenscreenman/MainActivity.java
@@ -12,6 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
     Button button;
     //static TextView textView;
--- app/src/main/java/in/inator/greenscreenman/arAcrivity.java
+++ app/src/main/java/in/inator/greenscreenman/arAcrivity.java
@@ -2,11 +2,17 @@ package in.inator.greenscreenman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class arAcrivity extends AppCompatActivity {
     private SurfaceView preview=null;
@@ -14,15 +20,92 @@ public class arAcrivity extends AppCompatActivity {
     private Camera camera=null;
     private boolean inPreview=false;
     private boolean cameraConfigured=false;
+    String model = "null";
+    GifImageView giffer;
+    LinearLayout top,bottom;
+    Button t1,t2,t3,t4,b1,b2,b3,b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar_acrivity);
-
+        Intent intent = getIntent();
+        model = intent.getExtras().getString("model");
         preview=(SurfaceView)findViewById(R.id.preview);
         previewHolder=preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        giffer = findViewById(R.id.giffer);
+        top = findViewById(R.id.lin_bt_top);
+        bottom = findViewById(R.id.lin_bt_bot);
+        t1 = findViewById(R.id.bt_t1);
+        t2 = findViewById(R.id.bt_t2);
+        t3 = findViewById(R.id.bt_t3);
+        t4 = findViewById(R.id.bt_t4);
+        b1 = findViewById(R.id.bt_b1);
+        b2 = findViewById(R.id.bt_b2);
+        b3 = findViewById(R.id.bt_b3);
+        b4 = findViewById(R.id.bt_b4);
+        if(model.equals("0")){
+            giffer.setImageResource(R.drawable.clap);
+            top.setVisibility(View.GONE);
+            bottom.setVisibility(View.GONE);
+            top.setEnabled(false);
+            bottom.setEnabled(false);
+        }else if(model.equals("1")){
+            top.setVisibility(View.VISIBLE);
+            bottom.setVisibility(View.VISIBLE);
+            giffer.setImageResource(R.drawable.i_idle);
+            top.setEnabled(true);
+            bottom.setEnabled(true);
+            t1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_idle);
+                }
+            });
+            t2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_falldown);
+                }
+            });
+            t3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_question);
+                }
+            });
+            t4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_spiderman);
+                }
+            });
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_falldown2);
+                }
+            });
+            b2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_kungfu);
+                }
+            });
+            b3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_naked);
+                }
+            });
+            b4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    giffer.setImageResource(R.drawable.i_wardrobe);
+                }
+            });
+        }
     }
 
     @Override
