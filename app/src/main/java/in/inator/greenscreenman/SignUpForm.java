57d23cbc9588ec50c6895cd5a3531e9bf52da14e
REXRITZ <rmbhadane33@gmail.com>
2020-07-10 15:09:21 +0530

    new UI

---
+++ app/src/main/java/in/inator/greenscreenman/SignUpForm.java
@@ -0,0 +1,14 @@
+package in.inator.greenscreenman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_form);
+    }
+}
